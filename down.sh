0df34ce063ce84da20b2e9ee429dbafbe6a00b00
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-10-12 14:18:51 +0530

    Down script update

--- down.sh
+++ down.sh
@@ -10,4 +10,6 @@ CHECK_CLUSTER_EXISTENCE=$(k3d cluster list | grep -c "$CLUSTER_NAME" || true)
 if [[ "$CHECK_CLUSTER_EXISTENCE" != "0" ]]
 then
   k3d cluster delete --config cluster.yml
+else
+  echo "Cluster is already deleted..."
 fi
