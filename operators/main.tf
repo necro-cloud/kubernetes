9d30027b6ea756a56d10f04904c8fcf013521ef7
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-10-12 13:58:41 +0530

    Cert Manager Operator

--- operators/main.tf
+++ operators/main.tf
@@ -9,3 +9,20 @@ resource "helm_release" "minio" {
   create_namespace = true
 
 }
+
+// Cert Manager Operator Configuration
+resource "helm_release" "cert-manager" {
+  name      = "cert-manager"
+  namespace = "cert-manager"
+
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "v1.16.1"
+  create_namespace = true
+
+  set {
+    name  = "crds.enabled"
+    value = true
+  }
+
+}
